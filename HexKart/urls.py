3b46ce0e4a1640a750376fd418c8efd423889d99
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2022-06-07 22:16:53 +0530

    changed the urls path

--- HexKart/urls.py
+++ HexKart/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Home/',include('Home.urls')),
+    path('',include('Home.urls')),
     path('Payment/',include('Payment.urls'))
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
